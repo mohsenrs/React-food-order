83c25b6bb8a5a8b8a877797d0414b010620f3e5e
mohsenrs <mohsen8022@gmail.com>
2021-12-19 10:09:56 +0330

    add meals

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,24 @@
+import classes from './MealsSummary.module.css'
+
+function MealsSummary() {
+  return (
+    <section className={classes.summary}>
+      <h2>
+        Lorem ipsum dolor sit amet consectetur.
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        consequatur esse ut eligendi enim est quidem. Sapiente error, quia
+        consequatur minus illum ducimus corporis voluptate ad porro, praesentium
+        aut vitae!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem pariatur
+        sunt laboriosam aspernatur nesciunt omnis eveniet tempore provident,
+        excepturi labore?
+      </p>
+    </section>
+  )
+}
+
+export default MealsSummary
